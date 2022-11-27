e134e838a1712f36a254a1871d62a2ce5b88aa98
Catherine-Song-303 <118871157+Catherine-Song-303@users.noreply.github.com>
2022-11-26 23:54:57 -0600

    Update main.py
    change some typos

--- main.py
+++ main.py
@@ -18,14 +18,15 @@ if __name__ == '__main__':
 while True:
     try:
         user_input = input('Please enter the command: ')
+        user_input = str(user_input).lower()
         if not user_input.isalpha():
             raise ValueNotAlpha
-        if user_input.isalpha() and user_input.lower() != 'w' and user_input.lower() != 's' \
-                and user_input.lower() != 'a' and user_input.lower() != 'd':
+        if user_input.isalpha() and user_input != 'w' and user_input != 's' \
+                and user_input != 'a' and user_input != 'd':
             raise ValueNotDesignated4Letter
 
         # 'w' = move up
-        if user_input == 'w' or user_input == 'W':
+        if user_input == 'w':
             matt, changes = logic.move_up(matt)
             status = logic.get_status(matt)
             print(status)
@@ -36,7 +37,7 @@ while True:
                 break
 
             # 's' = move down
-        elif user_input == 's' or user_input == 'S':
+        elif user_input == 's':
             matt, changes = logic.move_down(matt)
             status = logic.get_status(matt)
             print(status)
@@ -47,7 +48,7 @@ while True:
                 break
 
             # 'a' = move left
-        elif user_input == 'a' or user_input == 'A':
+        elif user_input == 'a':
             matt, changes = logic.move_left(matt)
             status = logic.get_status(matt)
             print(status)
@@ -58,7 +59,7 @@ while True:
                 break
 
             # 's' = move right
-        elif user_input == 'd' or user_input == 'D':
+        elif user_input == 'd':
             matt, changes = logic.move_right(matt)
             status = logic.get_status(matt)
             print(status)
